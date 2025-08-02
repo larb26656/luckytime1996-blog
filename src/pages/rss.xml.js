98bc0bbfc2ff6166ea73937852091fc1b5e66c4d
Luckytime1996 <larb26656@gmail.com>
2025-08-02 10:37:10 +0700

    feat: add web ring and implement filter non draft in post

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,14 +1,22 @@
 import rss from '@astrojs/rss';
-import { getCollection } from 'astro:content';
+import { getAllPosts } from '@/lib/repo/post.repo';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
 
 export async function GET(context) {
-	const posts = await getCollection('blog');
+	const allPosts = await getAllPosts({
+		criteria: {
+			draft: false,
+		},
+		sort: {
+			date: "DESC",
+		},
+	});
+
 	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: context.site,
-		items: posts.map((post) => ({
+		items: allPosts.map((post) => ({
 			...post.data,
 			link: `/blog/${post.id}/`,
 		})),
