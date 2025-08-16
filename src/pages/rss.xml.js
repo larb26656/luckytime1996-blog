dc3927c53bbba965191337104307f82084345696
Luckytime1996 <larb26656@gmail.com>
2025-08-16 23:55:17 +0700

    feat: add thumbnail and pubDate on rss style: adjust style in navbar and footer

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -5,13 +5,21 @@ import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
 export async function GET(context) {
 	const allPosts = (await getAllPosts()).slice(0, 10);
 
-	return rss({
+	const rssData = {
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: context.site,
 		items: allPosts.map((post) => ({
 			...post.data,
-			link: `/blog/${post.id}/`,
+			image: post.data.image?.src,
+			enclosure: {
+				url: post.data.image?.src,
+				length: 0,
+				type: `image/${post.data.image?.format}`
+			},
+			pubDate: post.data.date?.toDateString(),
 		})),
-	});
+	};
+
+	return rss(rssData);
 }
